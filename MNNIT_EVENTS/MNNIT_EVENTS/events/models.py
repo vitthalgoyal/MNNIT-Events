dfd1f451ba16118c123736e08a0ed8894b82f6e0
vitthalgoyal <vitthalgoyal17@gmail.com>
2020-10-20 17:44:27 +0530

    updates

---
+++ MNNIT_EVENTS/MNNIT_EVENTS/events/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phone_field import PhoneField
+from datetime import datetime
+
+# Create your models here.
+
+class fullUser(models.Model):
+
+    college_name = models.CharField(max_length=100)
+    phone_number = PhoneField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class events(models.Model):
+    
+    name = models.CharField(max_length=100)
+    img = models.ImageField()
+    des = models.TextField()
+    price = models.IntegerField()
+    start_date = models.DateField()
+    duration_days = models.PositiveIntegerField()
+
+class query(models.Model):
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length = 254)
+    subject = models.TextField()
+    message = models.TextField() 
+
+class collegeName(models.Model):
+    name = models.CharField(max_length=100)
+
+class campusAmbassador(models.Model):
+    name = models.CharField(max_length=100)
+    user_id = models.PositiveIntegerField()
+    event_id = models.PositiveIntegerField()
+    previous_experience = models.TextField()
+    approval = models.BooleanField(default=False)
+
+class ticket(models.Model):
+    name = models.CharField(max_length=100)
+    user_id = models.PositiveIntegerField()
+    event_name = models.CharField(max_length=100)
+    event_id = models.PositiveIntegerField()
+    college_name = models.CharField(max_length=100)
+    date_of_purchase = models.DateTimeField(auto_now_add=True)
+
+
