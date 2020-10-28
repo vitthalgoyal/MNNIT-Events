a57147eac85985ab19a53245a1c6fb429181bb50
vitthalgoyal <vitthalgoyal17@gmail.com>
2020-10-28 13:31:13 +0530

    Final Update

--- MNNIT_EVENTS/MNNIT_EVENTS/settings.py
+++ MNNIT_EVENTS/MNNIT_EVENTS/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
@@ -136,3 +136,11 @@ MEDIA_URL = '/media/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+#SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = 'vitthalgoyal2002@gmail.com'
+EMAIL_HOST_PASSWORD = 'Vitthal@14' 
--- MNNIT_EVENTS/events/urls.py
+++ MNNIT_EVENTS/events/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from . import views
 
 from django.conf import settings
@@ -17,6 +18,22 @@ urlpatterns = [
     path('sendQuery',views.sendQuery,name="sendQuery"),
     path('details/requestCA/<int:evId>',views.requestCA,name='requestCA'),
     path('details/userList/<int:evId>',views.userList,name='userList'),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="new_password_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_done.html"), 
+        name="password_reset_complete"),
 ]
 
 
