17ef4791c32bef2fe437968a3b961f237957fbf4
vitthalgoyal <vitthalgoyal17@gmail.com>
2020-11-03 15:13:32 +0530

    smtp email left

--- MNNIT_EVENTS/MNNIT_EVENTS/settings.py
+++ MNNIT_EVENTS/MNNIT_EVENTS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.auth import hashers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'vitthalgoyal2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'Vitthal@14' 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' 
